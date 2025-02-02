10af749bcb70bec4d6354ad8373647271e6ced70
Iluje <juliend1305@gmail.com>
2025-02-02 19:38:32 +0100

    Big change

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -42,52 +42,33 @@ namespace Game
         [ContextMenu("Think")]
         private void Think()
         {
-            // créer une liste de int
-            List<int> values = new List<int>();
-            
-            // récuper le node actuelle du plateau avec pour paramètre, Le plateau, qui joue le tour, et qui pense.
             Node currentNode = new Node(BoardsHandler.Instance.Pieces, isWhiteTurn, isWhiteTurn);
+
+            List<Node> children = currentNode.Children();
+            int bestValue = int.MinValue;
+            Node bestChild = null;
             
-            // Pour chaque enfant de ce Node
-            foreach (Node child in currentNode.Children())
-            { 
-                // stocker dans un int, le résulta de la mehide MinMax
-                int max = AiHandler.MinMax(child, 1, !child.IsWhiteTurn); 
-                
-                // l'ajouter dans la liste
-                values.Add(max);
-                
-                Debug.Log(" heur " + values.Count);
-                
-                // créer une variable int qui serra égal à - l'infini
-                int bestValue = int.MinValue;
-                Node bestChild = null;
-                // pour chaque valeur, dans Values
-                foreach (int value in values)
+            
+            foreach (Node child in children)
+            {
+                int value = child.HeursticValue();
+                if (value > bestValue)
                 {
-                    // si la value est supèrieur a la précédente 
-                    if (value > bestValue)
-                    {
-                        // alors la value est égal à la meilleur valeur
-                        bestValue = value;
-                        bestChild = child;
-                        
-                        Debug.Log("<color=red> Valeur du best child </color>" + bestChild.Value);
-                    }
+                    bestValue = value;
+                    bestChild = child;
                 }
-                
-                BoardsHandler.Instance.ResetMatrix();
-                BoardsHandler.Instance.Pieces = bestChild.Pieces;
-                BoardsHandler.Instance.DisplayMatrix();
-                
-                //BoardsHandler.Instance.DisplayMatrix();
-                
-                //Debug.Log("Heuristic : " + max + " child is ");
-                //Debug.Log("<color=red> Heuristic best Value </color>" + bestValue + " tour des joueurs blanc ?" + isWhiteTurn);
-                //BoardsHandler.Instance.Pieces = bestChild.Pieces;
-                
-                //Debug.Log("<color=green> Board count  </color> " + BoardsHandler.Instance.Pieces);
             }
+            BoardsHandler.Instance.ResetMatrix();
+            BoardsHandler.Instance.Pieces = bestChild.Pieces;
+            BoardsHandler.Instance.DisplayMatrix();
+
+            //BoardsHandler.Instance.DisplayMatrix();
+
+            //Debug.Log("Heuristic : " + max + " child is ");
+            //Debug.Log("<color=red> Heuristic best Value </color>" + bestValue + " tour des joueurs blanc ?" + isWhiteTurn);
+            //BoardsHandler.Instance.Pieces = bestChild.Pieces;
+
+            //Debug.Log("<color=green> Board count  </color> " + BoardsHandler.Instance.Pieces);
         }
     }
 }
