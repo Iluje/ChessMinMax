99414eb0fa4574758163980c4fe9546d9f75856b
Iluje <juliend1305@gmail.com>
2025-02-06 16:27:37 +0100

    Finish Depht 2

--- Assets/Scripts/Pieces/Pawn.cs
+++ Assets/Scripts/Pieces/Pawn.cs
@@ -18,124 +18,105 @@ namespace Pieces
         };
 
         public override List<Vector2Int> AvailableMovements(Vector2Int position, Piece[,] board)
+{
+    List<Vector2Int> movements = new List<Vector2Int>();
+
+    int i;
+
+    if (isWhite == false)
+    {
+        if (position.x == 1)
         {
-            List<Vector2Int> movements = new List<Vector2Int>();
-
-            int i;
-
-            if (isWhite == false)
+            for (i = position.x + 1; i <= position.x + 2; i++)
             {
-                if (position.x == 1)
+                if (i >= 0 && i < board.GetLength(0) && board[i, position.y] == null)
                 {
-                    for (i = position.x + 1; i <= position.x + 2; i++)
-                    {
-                        if (board[i, position.y] == null)
-                        {
-                            movements.Add(new Vector2Int(i, position.y));
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
+                    movements.Add(new Vector2Int(i, position.y));
                 }
                 else
                 {
-                    for (i = position.x + 1; i <= position.x + 1; i++)
-                    {
-                        if (board[i, position.y] == null)
-                        {
-                            movements.Add(new Vector2Int(i, position.y));
-                            continue;
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                }
-
-                foreach (Vector2Int direction in _directionsBlackEat)
-                {
-                    Vector2Int testDirection = position + direction;
-
-                    if (testDirection.x < 0 || testDirection.x > 7 || testDirection.y < 0 || testDirection.y > 7)
-                    {
-                        continue;
-                    }
-
-                    if (board[testDirection.x, testDirection.y] == null)
-                    {
-                        continue;
-                    }
-
-                    if (board[testDirection.x, testDirection.y].isWhite != isWhite)
-                    {
-                        movements.Add(new Vector2Int(testDirection.x, testDirection.y));
-                        continue;
-                    }
-
-                    continue;
+                    break;
                 }
             }
-            else
-            {
-                if (position.x == 6)
-                {
-                    for (i = position.x - 1; i >= position.x - 2; i--)
-                    {
-                        if (board[i, position.y] == null)
-                        {
-                            movements.Add(new Vector2Int(i, position.y));
-                            continue;
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                }
-                else
-                {
-                    for (i = position.x - 1; i >= position.x - 1; i--)
-                    {
-                        if (board[i, position.y] == null)
-                        {
-                            movements.Add(new Vector2Int(i, position.y));
-                            continue;
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                }
-
-                foreach (Vector2Int direction in _directionsWhiteEat)
-                {
-                    Vector2Int testDirection = position + direction;
-
-                    if (testDirection.x < 0 || testDirection.x > 7 || testDirection.y < 0 || testDirection.y > 7)
-                    {
-                        continue;
-                    }
-
-                    if (board[testDirection.x, testDirection.y] == null)
-                    {
-                        continue;
-                    }
-
-                    if (board[testDirection.x, testDirection.y].isWhite != isWhite)
-                    {
-                        movements.Add(new Vector2Int(testDirection.x, testDirection.y));
-                        continue;
-                    }
-
-                    continue;
-                }
-            }
-
-            return movements;
         }
+        else
+        {
+            for (i = position.x + 1; i <= position.x + 1; i++)
+            {
+                if (i >= 0 && i < board.GetLength(0) && board[i, position.y] == null)
+                {
+                    movements.Add(new Vector2Int(i, position.y));
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        foreach (Vector2Int direction in _directionsBlackEat)
+        {
+            Vector2Int testDirection = position + direction;
+            
+            if (testDirection.x >= 0 && testDirection.x < board.GetLength(0) && testDirection.y >= 0 && testDirection.y < board.GetLength(1))
+            {
+                if (board[testDirection.x, testDirection.y] == null || board[testDirection.x, testDirection.y].isWhite != isWhite)
+                {
+                    movements.Add(new Vector2Int(testDirection.x, testDirection.y));
+                }
+            }
+        }
+    }
+    else
+    {
+        if (position.x == 6)
+        {
+            for (i = position.x - 1; i >= position.x - 2; i--)
+            {
+                if (i >= 0 && i < board.GetLength(0) && board[i, position.y] == null)
+                {
+                    movements.Add(new Vector2Int(i, position.y));
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        else
+        {
+            for (i = position.x - 1; i >= position.x - 1; i--)
+            {
+                if (i >= 0 && i < board.GetLength(0) && board[i, position.y] == null)
+                {
+                    movements.Add(new Vector2Int(i, position.y));
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        foreach (Vector2Int direction in _directionsWhiteEat)
+        {
+            Vector2Int testDirection = position + direction;
+
+            // Vérifie que 'testDirection' est dans les limites de la grille
+            if (testDirection.x >= 0 && testDirection.x < board.GetLength(0) && testDirection.y >= 0 && testDirection.y < board.GetLength(1))
+            {
+                if (board[testDirection.x, testDirection.y] == null || board[testDirection.x, testDirection.y].isWhite != isWhite)
+                {
+                    movements.Add(new Vector2Int(testDirection.x, testDirection.y));
+                }
+            }
+        }
+    }
+
+    return movements;
+}
     }
 }
